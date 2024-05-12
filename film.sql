f405c8c39d721d59c16d2820ffc25761788680c9
MorvyExpected <153181833+MuhammadOscarFileas@users.noreply.github.com>
2024-05-12 14:12:35 +0700

    Add files via upload

---
+++ film.sql
@@ -0,0 +1,60 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Waktu pembuatan: 12 Bulan Mei 2024 pada 09.11
+-- Versi server: 10.4.32-MariaDB
+-- Versi PHP: 8.2.12
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `film`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `movie`
+--
+
+CREATE TABLE `movie` (
+  `judul` varchar(30) NOT NULL,
+  `alur` double NOT NULL,
+  `penokohan` double NOT NULL,
+  `akting` double NOT NULL,
+  `nilai` double NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data untuk tabel `movie`
+--
+
+INSERT INTO `movie` (`judul`, `alur`, `penokohan`, `akting`, `nilai`) VALUES
+('Arapaima', 2, 4.1, 5, 3.6999999999999997),
+('Book and shelv', 2, 4.2, 4, 3.4),
+('Jeruk', 4, 3, 1, 2.6666666666666665);
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indeks untuk tabel `movie`
+--
+ALTER TABLE `movie`
+  ADD UNIQUE KEY `judul` (`judul`);
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
